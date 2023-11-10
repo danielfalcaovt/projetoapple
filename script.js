81ccb4491c2d007147ec5f57eaf7bd4f680dfae7
falcaodaniel <danielfalcaoviktorio@gmail.com>
2023-11-09 21:03:37 -0300

    Search adicionado!

---
+++ script.js
@@ -0,0 +1,14 @@
+// AO CLICAR
+function searche(){
+let barrapesquisa = document.querySelector("div#srch")
+barrapesquisa.style.visibility = "visible"
+// VISIBILITY HIDDEN DA DIV #SRCH
+  
+// SE TRUE =FALSE
+// SE FALSE  =TRUE
+}
+function fechar() {
+    let barrapesquisa = document.querySelector("div#srch")
+barrapesquisa.style.visibility = "hidden"
+
+}
