0e2f666ef8d3e347589960c317de8fd651f7b7cf
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-02 17:02:20 -0300

    bug fixes

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ function searche(){
 let barrapesquisa = document.querySelector("div#srch")
 let menuu = document.querySelector("header")
 barrapesquisa.style.visibility = "visible"
+
+let nav = document.querySelector("nav")
+if (nav.classList[0] == "hdn"){
+}else{menu()}
 // VISIBILITY HIDDEN DA DIV #SRCH
-    menuu.style.visibility = "hidden"
+menuu.style.visibility = "hidden"
 // SE TRUE =FALSE
 // SE FALSE  =TRUE
 let pesquisa = document.querySelector("input#busca")
 //FUNÇAO MENU
-let nav = document.querySelector("nav")
-    nav.style.display = "flex"
-
 
 //DELAY ANTI-FLOODING
 setTimeout(() =>{
-pesquisa.style.display = "flex"
-pesquisa.style.animation = "abertura ease-in-out 1.1s"
+    pesquisa.style.display = "flex"
+
+
+        pesquisa.style.animation = "abertura ease-in-out 1.1s"
 pesquisa.focus()},80)
 }
 function fechar() {
@@ -25,14 +28,18 @@ function fechar() {
     menuu.style.visibility = "visible"
     let pesquisa = document.querySelector("input#busca")
     //FUNÇAO MENU
-    let nav = document.querySelector("nav")
     pesquisa.style.animation = ""
     pesquisa.style.animation = "fechamento ease-in-out 0.5s"
     
     //DELAY ANTI-FLOODING
     setTimeout(() =>{
+        let nav = document.querySelector("nav")
+        if (nav.classList[0] == "hdn"){
+       menu() }else{}
         barrapesquisa.style.visibility = "hidden"
-        nav.style.display = "none"
+        
+
+
         pesquisa.style.display = "none"
         
         
