890df61d0cacf3e2191e28e0734345acd0a4f615
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-07 07:34:23 -0300

    New update.
    jQuery added, modal added. smooth scroll...

--- script.js
+++ script.js
@@ -1,59 +1,84 @@
 // AO CLICAR
 function searche(){
-let barrapesquisa = document.querySelector("div#srch")
-let menuu = document.querySelector("header")
-barrapesquisa.style.visibility = "visible"
+let barrapesquisa = $('div#srch')
+let menuu = $("header")
+let pesquisa = $("input#busca")
+let nav = $("nav")
+nav.css('display','flex')
+barrapesquisa.slideToggle()
+barrapesquisa.css("display","flex")
+barrapesquisa.show()
 
-let nav = document.querySelector("nav")
-if (nav.classList[0] == "hdn"){
-}else{menu()}
-// VISIBILITY HIDDEN DA DIV #SRCH
-menuu.style.visibility = "hidden"
-// SE TRUE =FALSE
-// SE FALSE  =TRUE
-let pesquisa = document.querySelector("input#busca")
-//FUNÇAO MENU
-
-//DELAY ANTI-FLOODING
+modal.slideUp()
+menuu.css("visibility","hidden")
 setTimeout(() =>{
-    pesquisa.style.display = "flex"
-
-
-        pesquisa.style.animation = "abertura ease-in-out 1.1s"
-pesquisa.focus()},80)
+    
+    pesquisa.css("display","flex")
+        pesquisa.css("animation","abertura ease-in-out 1.1s")
+        pesquisa.focus()},80)
 }
+
+
+
 function fechar() {
-    let barrapesquisa = document.querySelector("div#srch")
-    let menuu = document.querySelector("header")
-    menuu.style.visibility = "visible"
-    let pesquisa = document.querySelector("input#busca")
-    //FUNÇAO MENU
-    pesquisa.style.animation = ""
-    pesquisa.style.animation = "fechamento ease-in-out 0.5s"
+    let barrapesquisa = $("div#srch")
+    let menuu = $("header")
+    let nav = $("nav")
+    nav.hide()
+    let pesquisa = $("input#busca")
+    
+    barrapesquisa.slideToggle()
+    menuu.css('visibility','visible')
+    pesquisa.css("animation","")
+    pesquisa.css("animation","fechamento ease-in-out 0.5s")
     
     //DELAY ANTI-FLOODING
     setTimeout(() =>{
-        let nav = document.querySelector("nav")
-        if (nav.classList[0] == "hdn"){
-       menu() }else{}
-        barrapesquisa.style.visibility = "hidden"
-        
-
-
-        pesquisa.style.display = "none"
+        barrapesquisa.css("display","none")
+        pesquisa.css("display","none")
         
         
     },400)
         
     }
     function menu(){
-        let nav = document.querySelector("nav")
-        let line = document.querySelector("div#firstline")
-        let line2 = document.querySelector("div#secondline")
-        line.classList.toggle('rotatex')
-        line2.classList.toggle("rotatey")        
-        nav.classList.toggle('hdn')
+        let nav = $("nav")
+        let line = $("#firstline")
+        let line2 = $("#secondline")
+        line.toggleClass('rotatex')
+        line2.toggleClass("rotatey")        
+        nav.slideToggle()
+        nav.css("display","flex")
        
 
 }
- 
+
+// Scroll top when click on imac link
+let scrolltop = $("a[href='#top']").click(function(){
+    $("html,body").animate({ scrollTop:0 }, "slow")
+})
+
+
+let modal = $(".topmodal")
+setTimeout(() => {
+    modal.slideDown()
+    modal.css("display","flex")
+
+}, 2000);
+    modal.click(function(){
+    modal.slideUp()
+})
+
+
+let alertclose = $(".alertclose")
+let alertmodal = $(".alertmodal")
+
+setTimeout(() => {
+    alertmodal.fadeIn()
+    alertmodal.css("display","flex")
+
+}, 5000);
+
+alertclose.click(function(){
+    alertmodal.fadeOut()
+})
